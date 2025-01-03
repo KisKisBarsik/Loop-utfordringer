9719bcb32b520f2c18b966e87e7b4c6a91c16c69
KisKisBarsik <sadisintick@gmail.com>
2025-01-03 15:16:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// Oppgave 1
+
+
+
+// function oppgave1() {
+
+//     let text = "røyken VGS"
+//     document.write("<table>")
+
+//     for (let i = 0; i < 3; i++) {
+
+//         document.write("<tr>")
+
+//         for (let j = 0; j < 3; j++) {
+
+//             document.write("<td>Royken VGS</td>")  
+
+//         }
+
+//         document.write("</tr>")
+        
+        
+//     }
+//     document.write("</table>")
+    
+// }
+
+// oppgave1()
+
+
+
+// Oppgave 2
+
+// function oppgave2() {
+
+//     let stjerne = "*"
+
+//     for (let i = 0; i < 10; i++) {
+        
+//         stjerne = " " + stjerne
+
+//         document.write(`<br>`)
+//         document.write(stjerne)
+        
+//     }
+    
+// }
+
+// oppgave2()
+
+
+
+// Oppgave 3
+
